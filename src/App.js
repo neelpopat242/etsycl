091ed24a0dde99264e7c79fc9187a37309768689
neelpopat242 <neelpopat242@gmail.com>
2022-06-04 19:04:39 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './templates/Header';
+import SpecialCategory from './templates/SpecialCategory';
+import SubCategory from './templates/SubCategory';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <SubCategory/>
+      <SpecialCategory/>
     </div>
   );
 }
---
+++ src/templates/SubCategory.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+export default function SubCategory() {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="row container-fluid">
+                    <div className="col ">
+                        <a className='nav-link text-center'>Jewellery & Accessories</a>
+                    </div>
+                    <div className="col">
+                        <a className='nav-link'>Clothing & Shoes</a>
+                    </div>
+                    <div className="col">
+                        <a className='nav-link'>Home & Living</a>
+                    </div>
+                    <div className="col">
+                        <a className='nav-link'>Wedding & Party</a>
+                    </div>
+                    <div className="col">
+                        <a className='nav-link'>Toys & Entertainment</a>
+                    </div>
+                    <div className="col">
+                        <a className='nav-link'>Art & Collectibles</a>
+                    </div>
+                    <div className="col">
+                        <a className='nav-link'>Vintage</a>
+                    </div>
+                    <div className="col">
+                        <a className='nav-link'>Gifts <span className="ec ec-gift"></span></a>
+                    </div>
+                    
+                   
+                </div>
+            </nav>
+        </div>
+    )
+}
