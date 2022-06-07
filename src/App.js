04b13d7da9cd2a782ed16a39fa65b2c924c74aae
neelpopat242 <neelpopat242@gmail.com>
2022-06-07 21:28:20 +0530

    temp setup home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
+import Footer from './templates/Footer';
 import Header from './templates/Header';
+import IntroFooter from './templates/IntroFooter';
+import OurSelection from './templates/OurSelection';
 import SpecialCategory from './templates/SpecialCategory';
 import SubCategory from './templates/SubCategory';
 
@@ -9,6 +12,10 @@ function App() {
       <Header/>
       <SubCategory/>
       <SpecialCategory/>
+      <OurSelection/>
+      <br/>
+      <IntroFooter/>
+      <Footer/>
     </div>
   );
 }
